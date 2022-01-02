76bb4c00b6848d13497c78a87ee3e36843e00fa6
CrimsonDart <79547580+CrimsonDart@users.noreply.github.com>
2022-01-02 03:02:39 -0600

    excvb

--- src/net/pgfmc/bot/player/ChatEvents.java
+++ src/net/pgfmc/bot/player/ChatEvents.java
@@ -39,7 +39,7 @@ public class ChatEvents implements Listener {
 		
 		e.setFormat(pd.getRankedName() + "§8 -> " + getMessageColor(e.getPlayer().getUniqueId().toString()) + e.getMessage());
 		
-		Discord.sendMessage(pd.getNickname(true) + " -> " + e.getMessage());
+		Discord.sendMessage(pd.getRankedName() + " -> " + e.getMessage());
 		// System.out.println(pd.getNickname(true));
 	}
 	
@@ -53,7 +53,7 @@ public class ChatEvents implements Listener {
 		}
 		
 		e.setJoinMessage("§7[§a+§7]§r " + pd.getRankedName());
-		Discord.sendMessage("<:JOIN:905023714213625886> " + pd.getNickname(true));
+		Discord.sendMessage("<:JOIN:905023714213625886> " + pd.getRankedName());
 		//Discord.sendEmbed(Discord.simplePlayerEmbed(pd.getPlayer(), "joined the server", Discord.green));
 	}
 	
@@ -63,7 +63,7 @@ public class ChatEvents implements Listener {
 		Player p = e.getPlayer();
 		
 		e.setQuitMessage("§7[§c-§7]§r " + PlayerData.getPlayerData(p).getRankedName());
-		Discord.sendMessage("<:LEAVE:905682349239463957> " + PlayerData.getPlayerData(p).getNickname(true));
+		Discord.sendMessage("<:LEAVE:905682349239463957> " + PlayerData.getPlayerData(p).getRankedName());
 		//Discord.sendEmbed(Discord.simplePlayerEmbed(p, "left the server", Discord.red));
 	}
 	
@@ -72,7 +72,7 @@ public class ChatEvents implements Listener {
 		PlayerData pd = PlayerData.getPlayerData(e.getEntity());
 		
 		String inDiscord = e.getDeathMessage().toString(); // ??? lololo
-		inDiscord.replace(pd.getName(), pd.getNickname(true));
+		inDiscord.replace(pd.getName(), pd.getRankedName());
 		
 		e.setDeathMessage(e.getDeathMessage().replace(pd.getName(), pd.getRankedName()));
 		
@@ -92,7 +92,7 @@ public class ChatEvents implements Listener {
 		if (adv.contains(" has made the advancement "))
 		{
 			adv = adv.substring(adv.lastIndexOf(pd.getName()), adv.length());
-			adv.replace(pd.getName(), pd.getNickname(true));
+			adv.replace(pd.getName(), pd.getRankedName());
 			
 			Discord.sendMessage(adv);
 			System.out.println("Advancement Get!"); // DO NOT REMOVE THIS!!!!!!!!!!!!!!!! (IT BREAKS) XXX lol
